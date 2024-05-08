89953746d3e803576af4510feac00d1734e5bfc8
angirar <rastogiangira0908@gmail.com>
2024-05-08 01:54:09 -0700

    Minor fix for new line in tex table (#8)
    * Minor fix for new line in tex table
    
    * Fix for underscore escape sequence
    
    ---------
    
    Co-authored-by: arastogi <angira.rastogi@cern.ch>

--- src/plotter/yields.py
+++ src/plotter/yields.py
@@ -4,11 +4,11 @@ from plotter import pdgRounding
 def print_yields_tex(title, hdata, hlistMC, ystr):
     s1, s2, s3, s4 = "", "", "", ""
     s1 = (
-        r"\\documentclass{article}\n\\usepackage{array}\n\\usepackage{graphicx} % "
-        r"for \\resizebox\n\\begin{document}\n\\begin{table}[htbp]\n\\centering\n\\caption{"
+        "\\documentclass{article}\n\\usepackage{array}\n\\usepackage{graphicx} % "
+        + "for \\resizebox\n\\begin{document}\n\\begin{table}[htbp]\n\\centering\n\\caption{"
         + title
-        + r"}\n\\resizebox{\\textwidth}{!}{%\n\\begin{tabular}{|c|c|c|c|c|}\n\\hline\nProcess "
-          r"& N\_Entries & Yield & Statistical unc. & Systematic unc.\\\\\n\\hline\n"
+        + "}\n\\resizebox{\\textwidth}{!}{%\n\\begin{tabular}{|c|c|c|c|c|}\n\\hline\nProcess "
+        + "& N\\_Entries & Yield & Statistical unc. & Systematic unc.\\\\\n\\hline\n"
     )
     hst_data = hdata.th
     entries = hst_data.GetEntries()
@@ -45,7 +45,7 @@ def print_yields_tex(title, hdata, hlistMC, ystr):
     s4 = (
         "\\hline\n\\end{tabular}%\n}\\label{tab:"
         + title
-        + "_yields_table}\n\\end{table}\n\\end{document}"
+        + "}\n\\end{table}\n\\end{document}"
     )
 
     ystr = s1 + s2 + s3 + s4
